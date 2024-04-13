77a95d50097b0bf7c34e2917238d72918dc75eef
Jonathan Poelen <jonathan.poelen@gmail.com>
2024-04-13 22:52:20 +0200

    [test] format output for easier reading of differences

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua
 
-function str(t)
+function str(t, prefix)
   if not t then
     return ''
   end
@@ -10,23 +10,46 @@ function str(t)
   end
   table.sort(orderedIndex)
 
-  local s, e = '{'
+  local s, e = '{\n'
   for k, i in pairs(orderedIndex) do
     e = t[i]
     if type(e) == 'table' then
-      e = (i ~= 'parent') and str(e) or e.tag
+      if i ~= 'parent' then
+        local has_elem = false
+        for _ in pairs(e) do
+          has_elem = true
+          break
+        end
+        e = has_elem and str(e, prefix .. '  ') or '{}'
+      else
+        e = e.tag
+      end
     end
-    s = s .. i .. ':' .. tostring(e) .. ','
+    s = s .. prefix .. '  ' .. i .. ': ' .. tostring(e) .. ',\n'
   end
-  return s .. '}'
+  return s .. prefix .. '}'
 end
 
 r = 0
 
 function check(tdoc, err, s, input, ierr, resultError)
-  local doc = str(tdoc)
+  local doc = str(tdoc, '  ')
   if resultError ~= err or s ~= doc then
-    (#arg ~= 0 and error or print)('[FAILURE]\n  ' .. s .. '\n  ==\n  ' .. doc .. '\n with ' .. input)
+    -- compute the position where the difference begins
+    local idiffer
+    for i=1,math.min(#s, #doc) + 1 do
+      if s:byte(i) ~= doc:byte(i) then
+        idiffer = i - 1
+        break
+      end
+    end
+
+    (#arg ~= 0 and error or print)('[FAILURE]\n  '
+      .. s:sub(1, idiffer) .. '\x1b[31m' .. s:sub(idiffer+1) .. '\x1b[m'
+      .. '\n  ==\n  '
+      .. doc:sub(1, idiffer) .. '\x1b[31m' .. doc:sub(idiffer+1) .. '\x1b[m'
+      .. '\n\n with ' .. input
+    )
     if err then print('  ' .. err .. '/' .. ierr) end
     print()
     r = r + 1
@@ -44,7 +67,7 @@ function _feq(parser, s, filename)
 end
 
 function _nopos(s)
-  return s:gsub(',pos:%d+', ''):gsub(' at position %d+', '')
+  return s:gsub('\n%s+pos: %d+,', ''):gsub(' at position %d+', '')
 end
 
 function mkEq(eq)
@@ -62,71 +85,590 @@ local eq, feq = mkEq(_eq), mkEq(_feq)
 xmllpegparser = require'xmllpegparser'
 
 -- empty file
-eq('{children:{},entities:{},lastpos:1,preprocessor:{},}',
+eq([[{
+    children: {},
+    entities: {},
+    lastpos: 1,
+    preprocessor: {},
+  }]],
    '')
 
 -- comment
-eq('{children:{},entities:{},lastpos:17,preprocessor:{},}',
+eq([[{
+    children: {},
+    entities: {},
+    lastpos: 17,
+    preprocessor: {},
+  }]],
    '<!-- comment -->')
 
 -- single text
-eq('{children:{1:{parent:nil,pos:1,text:abc,},},entities:{},lastpos:4,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        parent: nil,
+        pos: 1,
+        text: abc,
+      },
+    },
+    entities: {},
+    lastpos: 4,
+    preprocessor: {},
+  }]],
    'abc')
 
 -- single inline tag
-eq('{children:{1:{attrs:{},children:{},parent:nil,pos:1,tag:a,},},entities:{},lastpos:5,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {},
+        parent: nil,
+        pos: 1,
+        tag: a,
+      },
+    },
+    entities: {},
+    lastpos: 5,
+    preprocessor: {},
+  }]],
    '<a/>')
 
 -- single tag
-eq('{children:{1:{attrs:{},children:{1:{parent:a,pos:5,text:b,},},parent:nil,pos:1,tag:a,},},entities:{},lastpos:11,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            parent: a,
+            pos: 5,
+            text: b,
+          },
+        },
+        parent: nil,
+        pos: 1,
+        tag: a,
+      },
+    },
+    entities: {},
+    lastpos: 11,
+    preprocessor: {},
+  }]],
    '<a> b </a>')
 
 -- CDATA
-eq('{children:{1:{cdata:true,parent:nil,pos:1,text: xy &amp; ,},},entities:{},lastpos:23,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        cdata: true,
+        parent: nil,
+        pos: 1,
+        text:  xy &amp; ,
+      },
+    },
+    entities: {},
+    lastpos: 23,
+    preprocessor: {},
+  }]],
    '<![CDATA[ xy &amp; ]]>')
 
 -- CDATA with entity replacement
-eq('{children:{1:{cdata:true,parent:nil,pos:1,text: xy &amp; ,},},entities:{},lastpos:23,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        cdata: true,
+        parent: nil,
+        pos: 1,
+        text:  xy &amp; ,
+      },
+    },
+    entities: {},
+    lastpos: 23,
+    preprocessor: {},
+  }]],
    '<![CDATA[ xy &amp; ]]>', true)
 
-eq('{children:{1:{attrs:{},children:{},parent:nil,pos:1,tag:a,},2:{attrs:{},children:{1:{parent:b,pos:11,text:ad,},},parent:nil,pos:8,tag:b,},3:{attrs:{},children:{},parent:nil,pos:17,tag:c,},4:{attrs:{},children:{1:{attrs:{},children:{1:{parent:e,pos:27,text:ds,},},parent:d,pos:24,tag:e,},},parent:nil,pos:21,tag:d,},5:{attrs:{},children:{1:{parent:f,pos:40,text:a,},2:{attrs:{},children:{},parent:f,pos:41,tag:g,},3:{parent:f,pos:45,text:b,},},parent:nil,pos:37,tag:f,},},entities:{},lastpos:50,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {},
+        parent: nil,
+        pos: 1,
+        tag: a,
+      },
+      2: {
+        attrs: {},
+        children: {
+          1: {
+            parent: b,
+            pos: 11,
+            text: ad,
+          },
+        },
+        parent: nil,
+        pos: 8,
+        tag: b,
+      },
+      3: {
+        attrs: {},
+        children: {},
+        parent: nil,
+        pos: 17,
+        tag: c,
+      },
+      4: {
+        attrs: {},
+        children: {
+          1: {
+            attrs: {},
+            children: {
+              1: {
+                parent: e,
+                pos: 27,
+                text: ds,
+              },
+            },
+            parent: d,
+            pos: 24,
+            tag: e,
+          },
+        },
+        parent: nil,
+        pos: 21,
+        tag: d,
+      },
+      5: {
+        attrs: {},
+        children: {
+          1: {
+            parent: f,
+            pos: 40,
+            text: a,
+          },
+          2: {
+            attrs: {},
+            children: {},
+            parent: f,
+            pos: 41,
+            tag: g,
+          },
+          3: {
+            parent: f,
+            pos: 45,
+            text: b,
+          },
+        },
+        parent: nil,
+        pos: 37,
+        tag: f,
+      },
+    },
+    entities: {},
+    lastpos: 50,
+    preprocessor: {},
+  }]],
    '<a></a><b>ad</b><c/><d><e>ds</e></d><f>a<g/>b</f>')
 
-eq('{children:{1:{attrs:{name:value,},children:{},parent:nil,pos:1,tag:a,},2:{attrs:{name:value,},children:{},parent:nil,pos:18,tag:b,},3:{attrs:{name:value,},children:{},parent:nil,pos:41,tag:c,},4:{attrs:{name:value,name2:value2,},children:{},parent:nil,pos:60,tag:d,},},entities:{},lastpos:93,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {
+          name: value,
+        },
+        children: {},
+        parent: nil,
+        pos: 1,
+        tag: a,
+      },
+      2: {
+        attrs: {
+          name: value,
+        },
+        children: {},
+        parent: nil,
+        pos: 18,
+        tag: b,
+      },
+      3: {
+        attrs: {
+          name: value,
+        },
+        children: {},
+        parent: nil,
+        pos: 41,
+        tag: c,
+      },
+      4: {
+        attrs: {
+          name: value,
+          name2: value2,
+        },
+        children: {},
+        parent: nil,
+        pos: 60,
+        tag: d,
+      },
+    },
+    entities: {},
+    lastpos: 93,
+    preprocessor: {},
+  }]],
    '<a name="value"/><b   name  =  "value"/><c name="value"  /><d name="value"  name2="value2"/>')
 
-eq('{children:{1:{attrs:{name:v>a,},children:{},parent:nil,pos:1,tag:a,},2:{parent:nil,pos:16,text:> b,},3:{attrs:{name:>,},children:{1:{parent:c,pos:31,text:d,},},parent:nil,pos:19,tag:c,},4:{attrs:{name:a,},children:{1:{parent:e,pos:48,text:>f,},},parent:nil,pos:36,tag:e,},},entities:{},lastpos:54,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {
+          name: v>a,
+        },
+        children: {},
+        parent: nil,
+        pos: 1,
+        tag: a,
+      },
+      2: {
+        parent: nil,
+        pos: 16,
+        text: > b,
+      },
+      3: {
+        attrs: {
+          name: >,
+        },
+        children: {
+          1: {
+            parent: c,
+            pos: 31,
+            text: d,
+          },
+        },
+        parent: nil,
+        pos: 19,
+        tag: c,
+      },
+      4: {
+        attrs: {
+          name: a,
+        },
+        children: {
+          1: {
+            parent: e,
+            pos: 48,
+            text: >f,
+          },
+        },
+        parent: nil,
+        pos: 36,
+        tag: e,
+      },
+    },
+    entities: {},
+    lastpos: 54,
+    preprocessor: {},
+  }]],
    '<a name="v>a"/>> b<c name=">">d</c><e name="a">>f</e>')
 
 -- entity without replacement
-eq('{children:{1:{attrs:{},children:{1:{parent:a,pos:75,text:b,},},parent:nil,pos:72,tag:a,},},entities:{1:{name:e1,pos:29,value:fdd>d,},2:{name:e2,pos:53,value:a,},},lastpos:80,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            parent: a,
+            pos: 75,
+            text: b,
+          },
+        },
+        parent: nil,
+        pos: 72,
+        tag: a,
+      },
+    },
+    entities: {
+      1: {
+        name: e1,
+        pos: 29,
+        value: fdd>d,
+      },
+      2: {
+        name: e2,
+        pos: 53,
+        value: a,
+      },
+    },
+    lastpos: 80,
+    preprocessor: {},
+  }]],
    '<!DOCTYPE l SYSTEM "l.dtd"[ <!ENTITY e1   "fdd>d">  <!ENTITY e2 "a"> ]><a>b</a>')
 
 -- entity with replacement
-eq('{children:{1:{attrs:{},children:{1:{parent:a,pos:75,text:fdd>ddsa;,},},parent:nil,pos:72,tag:a,},},entities:{1:{name:e1,pos:29,value:fdd>d,},2:{name:e2,pos:53,value:a,},},lastpos:90,preprocessor:{},tentities:{amp:&,apos:\',e1:fdd>d,e2:a,gt:>,lt:<,nbsp: ,quot:",tab:\t,},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            parent: a,
+            pos: 75,
+            text: fdd>ddsa;,
+          },
+        },
+        parent: nil,
+        pos: 72,
+        tag: a,
+      },
+    },
+    entities: {
+      1: {
+        name: e1,
+        pos: 29,
+        value: fdd>d,
+      },
+      2: {
+        name: e2,
+        pos: 53,
+        value: a,
+      },
+    },
+    lastpos: 90,
+    preprocessor: {},
+    tentities: {
+      amp: &,
+      apos: ',
+      e1: fdd>d,
+      e2: a,
+      gt: >,
+      lt: <,
+      nbsp:  ,
+      quot: ",
+      tab: ]] .. '\t' .. [[,
+    },
+  }]],
    '<!DOCTYPE l SYSTEM "l.dtd" [<!ENTITY e1   "fdd>d">  <!ENTITY e2 "a"> ]><a>&e1;ds&e2;;</a>', true)
 
 -- missing closing tag
-eq('{children:{1:{attrs:{},children:{1:{parent:AA,pos:6,text:b,},},parent:nil,pos:1,tag:AA,},},entities:{},error:No matching closing tag for AA at position 1,lastpos:7,preprocessor:{},}',
+eq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            parent: AA,
+            pos: 6,
+            text: b,
+          },
+        },
+        parent: nil,
+        pos: 1,
+        tag: AA,
+      },
+    },
+    entities: {},
+    error: No matching closing tag for AA at position 1,
+    lastpos: 7,
+    preprocessor: {},
+  }]],
    '<AA> b', false, 'No matching closing tag for AA at position 1')
 
 -- closing tag does not match
-eq('{bad:{children:{1:{children:{},pos:8,tag:BB,},},},children:{1:{attrs:{},children:{1:{parent:AA,pos:6,text:b,},},parent:nil,pos:1,tag:AA,},},entities:{},error:No matching opening tag for BB at position 8,lastpos:13,preprocessor:{},}',
+eq([[{
+    bad: {
+      children: {
+        1: {
+          children: {},
+          pos: 8,
+          tag: BB,
+        },
+      },
+    },
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            parent: AA,
+            pos: 6,
+            text: b,
+          },
+        },
+        parent: nil,
+        pos: 1,
+        tag: AA,
+      },
+    },
+    entities: {},
+    error: No matching opening tag for BB at position 8,
+    lastpos: 13,
+    preprocessor: {},
+  }]],
    '<AA> b </BB>', false, 'No matching opening tag for BB at position 8')
 
 -- closing tag only
-eq('{bad:{children:{1:{children:{},pos:1,tag:BB,},},},children:{},entities:{},error:No matching opening tag for BB at position 1,lastpos:6,preprocessor:{},}',
+eq([[{
+    bad: {
+      children: {
+        1: {
+          children: {},
+          pos: 1,
+          tag: BB,
+        },
+      },
+    },
+    children: {},
+    entities: {},
+    error: No matching opening tag for BB at position 1,
+    lastpos: 6,
+    preprocessor: {},
+  }]],
    '</BB>', false, 'No matching opening tag for BB at position 1')
 
 -- closing tag then tag
-eq('{bad:{children:{1:{children:{},pos:1,tag:BB,},2:{attrs:{},children:{},parent:nil,pos:6,tag:a,},},},children:{},entities:{},error:No matching opening tag for BB at position 1,lastpos:13,preprocessor:{},}',
+eq([[{
+    bad: {
+      children: {
+        1: {
+          children: {},
+          pos: 1,
+          tag: BB,
+        },
+        2: {
+          attrs: {},
+          children: {},
+          parent: nil,
+          pos: 6,
+          tag: a,
+        },
+      },
+    },
+    children: {},
+    entities: {},
+    error: No matching opening tag for BB at position 1,
+    lastpos: 13,
+    preprocessor: {},
+  }]],
    '</BB><a></a>', false, 'No matching opening tag for BB at position 1')
 
 -- too many closing tag
-eq('{bad:{children:{1:{children:{},pos:12,tag:BB,},},},children:{1:{attrs:{},children:{1:{parent:a,pos:5,text:b,},},parent:nil,pos:1,tag:a,},},entities:{},error:No matching opening tag for BB at position 12,lastpos:17,preprocessor:{},}',
+eq([[{
+    bad: {
+      children: {
+        1: {
+          children: {},
+          pos: 12,
+          tag: BB,
+        },
+      },
+    },
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            parent: a,
+            pos: 5,
+            text: b,
+          },
+        },
+        parent: nil,
+        pos: 1,
+        tag: a,
+      },
+    },
+    entities: {},
+    error: No matching opening tag for BB at position 12,
+    lastpos: 17,
+    preprocessor: {},
+  }]],
    '<a> b </a> </BB>', false, 'No matching opening tag for BB at position 12')
 
-feq('{children:{1:{attrs:{},children:{1:{attrs:{attribute:&entity1;,},children:{1:{parent:lvl1,pos:185,text:something,},},parent:xml,pos:157,tag:lvl1,},2:{parent:xml,pos:204,text:blah blah,},3:{attrs:{attribute:value,},children:{},parent:xml,pos:216,tag:lvl1,},4:{attrs:{},children:{1:{attrs:{},children:{1:{parent:lvl2,pos:275,text:something,},},parent:other,pos:262,tag:lvl2,},},parent:xml,pos:250,tag:other,},},parent:nil,pos:149,tag:xml,},},entities:{1:{name:entity1,pos:88,value:something,},2:{name:entity2,pos:121,value:test,},},lastpos:315,preprocessor:{1:{attrs:{encoding:UTF-8,version:1.0,},pos:1,tag:xml,},},}',
+feq([[{
+    children: {
+      1: {
+        attrs: {},
+        children: {
+          1: {
+            attrs: {
+              attribute: &entity1;,
+            },
+            children: {
+              1: {
+                parent: lvl1,
+                pos: 185,
+                text: something,
+              },
+            },
+            parent: xml,
+            pos: 157,
+            tag: lvl1,
+          },
+          2: {
+            parent: xml,
+            pos: 204,
+            text: blah blah,
+          },
+          3: {
+            attrs: {
+              attribute: value,
+            },
+            children: {},
+            parent: xml,
+            pos: 216,
+            tag: lvl1,
+          },
+          4: {
+            attrs: {},
+            children: {
+              1: {
+                attrs: {},
+                children: {
+                  1: {
+                    parent: lvl2,
+                    pos: 275,
+                    text: something,
+                  },
+                },
+                parent: other,
+                pos: 262,
+                tag: lvl2,
+              },
+            },
+            parent: xml,
+            pos: 250,
+            tag: other,
+          },
+        },
+        parent: nil,
+        pos: 149,
+        tag: xml,
+      },
+    },
+    entities: {
+      1: {
+        name: entity1,
+        pos: 88,
+        value: something,
+      },
+      2: {
+        name: entity2,
+        pos: 121,
+        value: test,
+      },
+    },
+    lastpos: 315,
+    preprocessor: {
+      1: {
+        attrs: {
+          encoding: UTF-8,
+          version: 1.0,
+        },
+        pos: 1,
+        tag: xml,
+      },
+    },
+  }]],
    'example.xml')
 
 
@@ -140,7 +682,15 @@ parser = xmllpegparser.parser{
     return {tagnames=tags, err=err, pos=pos}
   end
 }
-_eq(parser, '{pos:12,tagnames:{1:a,2:b,},}', '<a><b/></a>')
+_eq(parser, [[{
+    pos: 12,
+    tagnames: {
+      1: a,
+      2: b,
+    },
+  }]],
+  '<a><b/></a>'
+)
 
 
 mkParser = function(...)
@@ -161,19 +711,61 @@ function peq(s, sxml)
   _eq(parser2, _nopos(s), sxml)
 end
 
-peq('{attrs:{},children:{1:{parent:x,pos:4,text:xxx/&y;,},},parent:nil,pos:1,tag:x,}',
+peq([[{
+    attrs: {},
+    children: {
+      1: {
+        parent: x,
+        pos: 4,
+        text: xxx/&y;,
+      },
+    },
+    parent: nil,
+    pos: 1,
+    tag: x,
+  }]],
     '<x>&x;/&y;</x>')
 
-peq('{attrs:{},children:{1:{parent:x,pos:51,text:xxx/yyy,},},parent:nil,pos:48,tag:x,}',
+peq([[{
+    attrs: {},
+    children: {
+      1: {
+        parent: x,
+        pos: 51,
+        text: xxx/yyy,
+      },
+    },
+    parent: nil,
+    pos: 48,
+    tag: x,
+  }]],
     '<!DOCTYPE l SYSTEM "l.dtd" [<!ENTITY y "yyy">]><x>&x;/&y;</x>')
 
-peq('{attrs:{},children:{},parent:nil,pos:20,tag:a,}',
+peq([[{
+    attrs: {},
+    children: {},
+    parent: nil,
+    pos: 20,
+    tag: a,
+  }]],
     '<!DOCTYPE language><a></a>')
 
-peq('{attrs:{},children:{},parent:nil,pos:22,tag:a,}',
+peq([[{
+    attrs: {},
+    children: {},
+    parent: nil,
+    pos: 22,
+    tag: a,
+  }]],
     '<!DOCTYPE language[]><a></a>')
 
-peq('{attrs:{},children:{},parent:nil,pos:39,tag:a,}',
+peq([[{
+    attrs: {},
+    children: {},
+    parent: nil,
+    pos: 39,
+    tag: a,
+  }]],
     '<!DOCTYPE language[<!ENTITY y "yyy">]><a></a>')
 
 do
@@ -195,13 +787,34 @@ do
   end
 end
 
-doctypeEq('{name:language,}', '<!DOCTYPE language>')
-doctypeEq('{name:language,}', '<!DOCTYPE language[]>')
-doctypeEq('{name:language,}', '<!DOCTYPE language[] >')
-doctypeEq('{cat:SYSTEM,name:language,path:language.dtd,}',
-          '<!DOCTYPE language SYSTEM "language.dtd">')
-doctypeEq('{cat:SYSTEM,name:language,path:language.dtd,}',
-          '<!DOCTYPE language SYSTEM "language.dtd"[]>')
+doctypeEq([[{
+    name: language,
+  }]],
+  '<!DOCTYPE language>')
+
+doctypeEq([[{
+    name: language,
+  }]],
+  '<!DOCTYPE language[]>')
+
+doctypeEq([[{
+    name: language,
+  }]],
+  '<!DOCTYPE language[] >')
+
+doctypeEq([[{
+    cat: SYSTEM,
+    name: language,
+    path: language.dtd,
+  }]],
+  '<!DOCTYPE language SYSTEM "language.dtd">')
+
+doctypeEq([[{
+    cat: SYSTEM,
+    name: language,
+    path: language.dtd,
+  }]],
+  '<!DOCTYPE language SYSTEM "language.dtd"[]>')
 
 if 0 == r then
   print('No error')
